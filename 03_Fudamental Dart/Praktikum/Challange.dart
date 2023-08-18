eeee717a6b60e0d9f50fed840ed4dfc944f54e2a
Avocodo-chp <annisayunia18@gmail.com>
2023-08-18 16:16:00 +0700

    challange

---
+++ 03_Fudamental Dart/Praktikum/Challange.dart
@@ -0,0 +1,23 @@
+//buatlah fungsi keliling persegi panjang dan segitiga
+
+int p = 5;
+int l = 8 ;
+
+int keliling_persegi_panjang = 2*p + 2*l;
+
+int a = 10;
+int b = 5 ;
+int c = 5 ;
+
+int segitiga = a + b +c ;
+
+void main(){
+  print('panjang : $p');
+  print('lebar : $l');
+  print('keliling Persegi Panjang: $keliling_persegi_panjang');
+  
+  print('Sisi A : $a');
+  print('Sisi B : $b');
+  print('Sisi C : $c');
+  print('Keliling segitiga : $segitiga');
+}
