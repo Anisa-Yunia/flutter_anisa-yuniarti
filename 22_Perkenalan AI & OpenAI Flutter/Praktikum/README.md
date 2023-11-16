6e6a01aa1e061948f803c4487c388ad967b5a607
Anisa-yuniarti <annisayunia18@gmail.com>
2023-11-17 00:09:18 +0700

    Open AI playground dan  AI recommendation

---
+++ 22_Perkenalan AI & OpenAI Flutter/Praktikum/README.md
@@ -0,0 +1,2 @@
+Link google docs tugas AI introdution :
+https://docs.google.com/document/d/1hnAv6exBWSsU6l4Rt3KBjCdi1ERMAdP1Xf3_5ot8a4k/edit?usp=sharing
---
+++ 23_OpenAi in Flutter/Praktikum/open_ai/lib/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+final class _Env {
+  static const String apiKey =
+      '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';
+}
